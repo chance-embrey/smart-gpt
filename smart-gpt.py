fe2c136f83a5eed17b32a136e20486c570cd40c0
Chance <99685123+chance-embrey@users.noreply.github.com>
2023-05-07 17:56:38 -0500

    Updated README instructions; Added support to use different GPT models

--- smart-gpt.py
+++ smart-gpt.py
@@ -6,11 +6,12 @@ from typing import List
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+gpt_model = os.getenv("GPT_MODEL")
 
 
-def generate_response(prompt: str, model: str = "gpt-4") -> str:
+def generate_response(prompt: str) -> str:
     response = openai.ChatCompletion.create(
-        model=model,
+        model=gpt_model,
         messages=[{
             "role": "user", "content": prompt
         }],
@@ -34,12 +35,12 @@ def chain_of_thought_prompting(question: str) -> str:
 def reflection_and_dialogue(question: str, response: str, max_rounds: int = 3) -> str:
     current_round = 1
     while current_round <= max_rounds:
-        print(f"Round {current_round}: Reflecting and dialoguing...")
+
         reflection_prompt = f"I answered the question '{question}' with the response '{response}'. Is this response correct? If yes, reply exactly 'Yes, this response is correct'. If not, what is the correct answer?"
         reflection_response = generate_response(reflection_prompt)
 
         print(
-            "Reflection response round {current_roud}/{max_rounds}: ", reflection_response)
+            f"Round {current_round}/{max_rounds} reflextion and dialogue: ", reflection_response)
 
         if "yes, this response is correct" in reflection_response.lower():
             break
@@ -60,7 +61,7 @@ def smart_gpt(question: str) -> str:
 def main():
     question = input("What is your question?\n")
     response = smart_gpt(question)
-    print(f"Smart GPT Response: \n{response}")
+    print(f"\n\nSmart GPT Response: \n{response}")
 
 
 main()
